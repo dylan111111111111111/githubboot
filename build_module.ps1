ff2b151155b960179867d3798686f090c06c65be
dylan111111111111111 <cloudfareversion4@gmail.com>
2026-03-26 17:39:09 -0700

    Add files via upload

---
+++ build_module.ps1
@@ -0,0 +1,5 @@
+param([string]$Dir="module_template",[string]$Out="my_module.zip")
+if (Test-Path $Out) { Remove-Item $Out }
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::CreateFromDirectory($Dir,$Out)
+Write-Host "Module ZIP: $Out"
